07194a3d36a295970bfae5ce6ce59e5dc48eab3c
SORAAAAAAAAA <rcbriones95@gmail.com>
2026-01-15 09:24:06 +0800

    fixed api call

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,7 +3,7 @@ const api = process.env.NEXT_PUBLIC_API_URL;
 // Get Users from database
 export const getUsers = async () => {
 
-    const response = await fetch(`${api}/users`);
+    const response = await fetch(`${api}/api/users`);
 
     if (!response.ok) {
         throw new Error('Failed to fetch users');
